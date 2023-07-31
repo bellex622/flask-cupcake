e890919e2210fe84310e94bd5cee0e2121aac870
Belle Xu <bellex22@protonmail.com>
2023-07-31 17:09:32 -0400

    done with first code review

--- app.py
+++ app.py
@@ -1,1 +1,76 @@
 """Flask app for Cupcakes"""
+from flask import Flask, redirect, render_template, jsonify, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+
+@app.get("/api/cupcakes")
+def list_all_cupcakes():
+    """get info about all cupcakes
+    return JSON: {cupcakes: [{id, flavor, size, rating, image_url}, ...]}
+    """
+
+    cupcakes = Cupcake.query.all()
+    print('all cupcakses---------->', cupcakes)
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake_detail(cupcake_id):
+    """get info about a single cupcake
+    return JSON:{cupcake: {id, flavor, size, rating, image_url}}
+    """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    """create a cupcake with details
+    Sample request with JSON:
+    {"flavor": "lemon","image_url": "","rating": 9,"size": "large"}
+
+    Respond with JSON:
+    {cupcake: {id, flavor, size, rating, image_url}}
+    """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image_url = request.json["image_url"] or None
+
+    new_cupcake = Cupcake(
+        flavor=flavor,
+        size=size,
+        rating=rating,
+        image_url=image_url)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
